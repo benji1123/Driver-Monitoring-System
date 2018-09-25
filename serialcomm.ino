2ec965107b9a6ee1522720e3b047ee261f5e375f
Jarvis Wang <acejarvis98@gmail.com>
2018-09-25 02:55:42 -0400

    Update hands-on analysis

--- serialcomm.ino
+++ serialcomm.ino
@@ -1,23 +1,27 @@
-char serialData;
-int pin=10;
-
-
+byte buttonPin[12] = {0,1,2,3,4,5,6,7,8,9,10,11};//initialize I/O ports
 
 void setup() {
-  pinMode(pin,OUTPUT);
-  Serial.begin(9600);
+  Serial.begin(9600);//setup serial communication
+  for (byte i = 0;i < 12;i++){
+    pinMode(buttonPin[i], INPUT_PULLUP);
+  }// setup button pins
 }
 
 void loop() {
-  if(Serial.available() > 0){
-    serialData = Serial.read();
-    Serial.print(serialData);
+  
+ int distractionStatus = distractionTest();// read the analysis result
+ Serial.print(distractionStatus);
+ Serial.println("");
+   delay(40);// define the fresh rate at 25Hz
+}
 
-    if(serialData == '1'){
-      digitalWrite(pin, HIGH);
-    }
-    else if(serialData == '0'){
-      digitalWrite(pin, LOW);
-    }
+//distraction analysis
+int distractionTest(){
+  byte buttonValue[3] = {5,6,7};
+  int logic=0;
+   for (byte i = 5;i < 8;i++){
+       buttonValue[i] = 1 - digitalRead(buttonPin[i]);
+       logic += buttonValue[i];  
   }
+  return logic;
 }
