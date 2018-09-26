e4b54dd05d0b62df0dfc7af8cade1904b392d1f4
Jarvis Wang <acejarvis98@gmail.com>
2018-09-26 16:27:35 -0400

    Update LED alarm

--- serialcomm/serialcomm.ino
+++ serialcomm/serialcomm.ino
@@ -1,18 +1,21 @@
-/*12 buttons version
- * output a distraction level according to driver's hands_on status
- * updated on Tue 5:18AM by Jarvis
+/*8 buttons version
+ * output a distraction level through LED according to driver's hands_on status
+ * updated on Wed 4:20PM by Jarvis
  */
 byte buttonPin[8]= {1,2,3,4,5,6,7};//initialize I/O ports
+byte ledPin[3]={9,10,11};//initialize I/O ports
 int dp = 0;//counter
-int handsOnAlert = 0;//LED alarm switch
-
+int handsOnTest(void);
+void alarm(int color);
 void setup() {
     Serial.begin(9600);//setup serial communication
     for (byte i = 0;i < 8;i++){
     pinMode(buttonPin[i], INPUT_PULLUP);
      }
-    pinMode(13,OUTPUT);//onboard LED
-    pinMode(9, OUTPUT);//LED alarm
+    //pinMode(13,OUTPUT);//onboard LED
+    for(byte i = 9;i < 12;i++){
+    pinMode(ledPin[i], OUTPUT);//LED alarm
+    }
  // setup button pins
 }
 
@@ -21,22 +24,20 @@ void loop() {
     int wheel_score = handsOnTest();// read the analysis result
     
     //detect if the driver's hands are not on the wheel for a while
-    if(wheel_score == 0) dp++;
+    if(wheel_score < 2) dp++;
     else dp = 0;
-    if(dp>100&&dp<=300) handsOnAlert = 1;// hands not on wheel over 4 seconds
+    if(dp>100&&dp<=300) alarm(wheel_score);  // hands not on wheel over 4 seconds
     
     else if(dp>300) dp=0;//clear the cache
-    else handsOnAlert = 0;
+    else alarm(2);
    
-    //turn on wheel_alarm
-    if(handsOnAlert>0) alarm();
-    else digitalWrite(9,LOW);
     //receive eyetracking result
-    if(Serial.available()>0){
-      if(Serial.read() == 2) alarm();
-    }
+    /*if(Serial.available()>0){
+      if(Serial.read() == 2) alarm(2);
+    */  
     
-    Serial.println(handsOnAlert);//output the distraction status from 0 - 2
+    
+    Serial.println(wheel_score);//output the distraction status from 0 - 2
     delay(40);// define the fresh rate at 25Hz
 }
 
@@ -54,11 +55,10 @@ int handsOnTest(){
       wheel_score = 0;//distraction
       break;
     case 1: 
-    case 2:  
-    case 3:
       wheel_score = 1;//normal focus
       break;
-      
+    case 2:  
+    case 3:
     case 4:
     case 5:  
     case 6:
@@ -70,13 +70,29 @@ int handsOnTest(){
   return wheel_score;
 }
 
-void alarm(){
-  digitalWrite(9, HIGH);
+void alarm(int color){
+  int color_pin = 20;
+  switch(color){
+    case 0:
+      color_pin = 9;//distraction, red
+      break;
+    case 1:
+      color_pin = 10;//moderate, yellow
+      break; 
+    case 2: 
+      color_pin = 11;//both hands on, green
+      break;
+  }
+
+  if(color<2){
+  digitalWrite(ledPin[color_pin], HIGH);
   delay(150);
-  digitalWrite(9, LOW);
+  digitalWrite(ledPin[color_pin], LOW);
   delay(150);
-  digitalWrite(9, HIGH);
+  digitalWrite(ledPin[color_pin], HIGH);
   delay(150);
-  digitalWrite(9, LOW);
+  digitalWrite(ledPin[color_pin], LOW);
   delay(150);
+  }
+  else digitalWrite(ledPin[color_pin], HIGH);
 }
