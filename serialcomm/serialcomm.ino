5cb2a3ffe65681041ea2a51a26b0a7c5fb8915e3
Jarvis Wang <acejarvis98@gmail.com>
2018-09-28 12:46:31 -0400

    Updated 3 LEDS alarm

--- serialcomm/serialcomm.ino
+++ serialcomm/serialcomm.ino
@@ -1,45 +1,10 @@
 /*8 buttons version
- * output a distraction level through LED according to driver's hands_on status
- * updated on Wed 4:20PM by Jarvis
+ * output a distraction level according to driver's hands_on status
+ * 3 LEDs Alarm
+ * updated on Fri 12:45PM by Jarvis
  */
-byte buttonPin[8]= {1,2,3,4,5,6,7};//initialize I/O ports
-byte ledPin[3]={9,10,11};//initialize I/O ports
+byte buttonPin[8]= {0,1,2,3,4,5,6,7};//initialize I/O ports
 int dp = 0;//counter
-int handsOnTest(void);
-void alarm(int color);
-void setup() {
-    Serial.begin(9600);//setup serial communication
-    for (byte i = 0;i < 8;i++){
-    pinMode(buttonPin[i], INPUT_PULLUP);
-     }
-    //pinMode(13,OUTPUT);//onboard LED
-    for(byte i = 9;i < 12;i++){
-    pinMode(ledPin[i], OUTPUT);//LED alarm
-    }
- // setup button pins
-}
-
-void loop() {
-  
-    int wheel_score = handsOnTest();// read the analysis result
-    
-    //detect if the driver's hands are not on the wheel for a while
-    if(wheel_score < 2) dp++;
-    else dp = 0;
-    if(dp>100&&dp<=300) alarm(wheel_score);  // hands not on wheel over 4 seconds
-    
-    else if(dp>300) dp=0;//clear the cache
-    else alarm(2);
-   
-    //receive eyetracking result
-    /*if(Serial.available()>0){
-      if(Serial.read() == 2) alarm(2);
-    */  
-    
-    
-    Serial.println(wheel_score);//output the distraction status from 0 - 2
-    delay(40);// define the fresh rate at 25Hz
-}
 
 //handsOn analysis
 int handsOnTest(){
@@ -65,6 +30,7 @@ int handsOnTest(){
     case 7:
       wheel_score = 2;//both hands on
       break; 
+      
   }
   
   return wheel_score;
@@ -85,14 +51,52 @@ void alarm(int color){
   }
 
   if(color<2){
-  digitalWrite(ledPin[color_pin], HIGH);
+  digitalWrite(color_pin, HIGH);
   delay(150);
-  digitalWrite(ledPin[color_pin], LOW);
+  digitalWrite(color_pin, LOW);
   delay(150);
-  digitalWrite(ledPin[color_pin], HIGH);
+  digitalWrite(color_pin, HIGH);
   delay(150);
-  digitalWrite(ledPin[color_pin], LOW);
+  digitalWrite(color_pin, LOW);
   delay(150);
   }
-  else digitalWrite(ledPin[color_pin], HIGH);
+  else digitalWrite(color_pin, HIGH);
+  
+  
 }
+
+
+void setup() {
+    Serial.begin(9600);//setup serial communication
+    for (byte i = 0;i < 8;i++){
+    pinMode(buttonPin[i], INPUT_PULLUP);
+     }
+    //pinMode(13,OUTPUT);//onboard LED
+    for(byte i = 9;i < 12;i++){
+    pinMode(i, OUTPUT);//LED alarm
+    }
+ // setup button pins
+}
+
+void loop() {
+  
+    int wheel_score = handsOnTest();// read the analysis result
+    
+    //detect if the driver's hands are not on the wheel for a while
+    if(wheel_score < 2) dp++;
+    else dp = 0;
+    if(dp>100&&dp<=300) {alarm(wheel_score);
+    digitalWrite(11,LOW);}// hands not on wheel over 4 seconds
+    else if(dp>300) dp=0;//clear the cache
+    else alarm(2);
+   
+    //receive eyetracking result
+    /*if(Serial.available()>0){
+      if(Serial.read() == 2) alarm(2);
+    */  
+    
+    
+    Serial.println(wheel_score);//output the distraction status from 0 - 2
+    delay(40);// define the fresh rate at 25Hz
+}
+
